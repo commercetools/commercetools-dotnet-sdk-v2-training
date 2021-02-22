de57f7023906deeee19b98950bd9d38e8006a97a
Michele George <michele.george@commercetools.de>
2021-02-22 11:56:46 +0100

    Upgrade the SDK Packages and use the ImportAPI Package from nuget

--- Training/Exercises/Task06b_PAGEDQUERY.cs
+++ Training/Exercises/Task06b_PAGEDQUERY.cs
@@ -2,7 +2,6 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using commercetools.Api.Client;
-using commercetools.Api.Models.Products;
 using commercetools.Base.Client;
 
 namespace Training
@@ -15,7 +14,6 @@ namespace Training
     {
         private readonly IClient _client;
         
-
         public Task06B(IClient client)
         {
             this._client = client;
@@ -24,31 +22,19 @@ namespace Training
         public async Task ExecuteAsync()
         {
             string lastId = null ;int pageSize = 2;int currentPage = 1; bool lastPage = false;
-            ProductPagedQueryResponse response = null;
 
             while (!lastPage)
             {
-                if (lastId == null)
-                {
-                    response = await _client.WithApi().WithProjectKey(Settings.ProjectKey)
-                        .Products()
-                        .Get()
-                        .WithSort("id asc")
-                        .WithLimit(pageSize)
-                        .WithWithTotal(false)
-                        .ExecuteAsync();
-                }
-                else
-                {
-                    response = await _client.WithApi().WithProjectKey(Settings.ProjectKey)
-                        .Products()
-                        .Get()
-                        .WithSort("id asc")
-                        .WithLimit(pageSize)
-                        .WithWhere($"id>\"{lastId}\"")
-                        .WithWithTotal(false)
-                        .ExecuteAsync();
-                }
+                var where = lastId != null ? $"id>\"{lastId}\"" : null;
+                
+                var response = await _client.WithApi().WithProjectKey(Settings.ProjectKey)
+                    .Products()
+                    .Get()
+                    .WithSort("id asc")
+                    .WithLimit(pageSize)
+                    .WithWhere(where)
+                    .WithWithTotal(false)
+                    .ExecuteAsync();
 
                 Console.WriteLine($"Show Results of Page {currentPage}");
                 foreach (var product in response.Results)
