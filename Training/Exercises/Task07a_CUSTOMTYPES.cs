45602ffe16a1338c59a580c0d41576ec9a35d0bd
Nagesh Dixit <nagesh.dixit@commercetools.com>
2022-02-16 12:33:20 -0500

    fixed customType creation task

--- Training/Exercises/Task07a_CUSTOMTYPES.cs
+++ Training/Exercises/Task07a_CUSTOMTYPES.cs
@@ -10,8 +10,8 @@ using Training.Services;
 namespace Training
 {
     /// <summary>
-    /// 1- Create TypeDraft with Custom fields
-    /// 2- Create The Type and assign it to customers (as Resources you want to extend)
+    /// Creates the custom type with a field 'allowed-to-place-orders'
+    /// </summary>
     public class Task07A : IExercise
     {
         private readonly IClient _client;
@@ -25,39 +25,21 @@ namespace Training
 
         public async Task ExecuteAsync()
         {
-
-            var typeDraft = new TypeDraft
-            {
-                Key = "allowed-to-place-orders",
-                Name = new LocalizedString {{"de", "allowed-to-place-orders"},{"en", "allowed-to-place-orders"}},
-                ResourceTypeIds = new List<IResourceTypeId> {IResourceTypeId.Customer},
-                FieldDefinitions = new List<IFieldDefinition>
-                {
-                    this.CreateTypeFieldDefinition()
-                }
-            };
-
             var createdType = await _typeService.CreateCustomType("allowed-to-place-orders", 
                 new LocalizedString {{"de", "allowed-to-place-orders"},{"en", "allowed-to-place-orders"}},
                 new List<IResourceTypeId> {IResourceTypeId.Customer},
                 new List<IFieldDefinition>
                 {
-                    this.CreateTypeFieldDefinition()
+                    new FieldDefinition
+                    {
+                        Name = "allowed-to-place-orders",
+                        Required = false,
+                        Label = new LocalizedString {{"de", "Allowed to place orders"},{"en", "Allowed to place orders"}},
+                        Type = new CustomFieldBooleanType()
+                    }
                 }
             );
             Console.WriteLine($"New custom type has been created with Id: {createdType.Id}");
-        }
-
-        private FieldDefinition CreateTypeFieldDefinition()
-        {
-            var fieldDefinition = new FieldDefinition
-            {
-                Name = "allowed-to-place-orders",
-                Required = false,
-                Label = new LocalizedString {{"de", "Allowed to place orders"},{"en", "Allowed to place orders"}},
-                Type = new CustomFieldBooleanType()
-            };
-            return fieldDefinition;
         }
     }
 }
